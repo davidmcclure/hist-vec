96ce880ecd9a1f5aad09cdef05da7b6c91e42e6c
David McClure <davidwilliammcclure@gmail.com>
2017-02-22 07:06:51 -0800

    Add setup.py and setup-sherlock.

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='hist-vec',
+    version='0.1.0',
+    description='How do words change over time?',
+    url='https://github.com/davidmcclure/hist-vec',
+    license='MIT',
+    author='David McClure',
+    author_email='dclure@stanford.edu',
+    packages=find_packages(),
+)
