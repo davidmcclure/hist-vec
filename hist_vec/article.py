98a36ab50cf93f52c551cddf70034b0d468569c8
David McClure <davidwilliammcclure@gmail.com>
2017-02-22 08:10:34 -0800

    Downcase tokens.

--- hist_vec/article.py
+++ hist_vec/article.py
@@ -32,4 +32,11 @@ class Article:
         doc = self.nlp(self.data['text'] or '')
 
         for sent in doc.sents:
-            yield list(map(str, doc[sent.start:sent.end]))
+
+            tokens = doc[sent.start:sent.end]
+
+            # Convert to lowercase string.
+            tokens = map(str, tokens)
+            tokens = map(str.lower, tokens)
+
+            yield list(tokens)
